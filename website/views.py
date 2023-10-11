f3008ff630603be471e68d603b2525eca8614351
nguyenloc710 <loc7102010@gmail.com>
2023-10-11 16:56:56 +0700

    done

--- website/views.py
+++ website/views.py
@@ -1,9 +1,37 @@
-from flask import Blueprint, render_template
-from flask_login import login_user, login_required, logout_user, current_user
+import json
+
+from flask import Blueprint, render_template, request, flash, jsonify
+from flask_login import login_required, current_user
+
+from website import db
+from website.models import Note
 
 views = Blueprint('views', __name__)
 
-@views.route('/')
+
+@views.route('/', methods=['GET', 'POST'])
 @login_required
 def home():
+    if request.method == 'POST':
+        note = request.form.get('note')
+        if len(note) < 1:
+            flash('Note is to short!', catetory='error')
+        else:
+            new_note = Note(data=note, user_id=current_user.id)
+            db.session.add(new_note)
+            db.session.commit()
+            flash('Note added!', category='success')
     return render_template("home.html", user=current_user)
+
+
+@views.route('/delete-note', methods=['POST'])
+def delete_note():
+    note = json.loads(request.data)
+    note_id = note['noteId']
+    note = Note.query.get(note_id)
+    if note:
+        if note.user_id == current_user.id:
+            db.session.delete(note)
+            db.session.commit()
+
+    return jsonify({})
